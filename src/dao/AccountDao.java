e62e72322591917adf814f20dad7a42c24ed462c
sayssy <445463692@qq.com>
2018-05-09 14:19:00 +0800

    2018-5-9 update

--- src/dao/AccountDao.java
+++ src/dao/AccountDao.java
@@ -1,5 +1,71 @@
 package dao;
 
-public class AccountDao {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import data.*;
+
+public class AccountDao extends BaseDao {
+	public AccountDao() {
+		// TODO Auto-generated constructor stub
+		super();
+	}
+	public boolean addAccount(Account acc) {
+		String sql = "insert into Account values(?, ?, ?, ?)";
+		try {
+			Connection con = super.getConnection();
+			PreparedStatement stmt = con.prepareStatement(sql);
+			stmt.setString(1, acc.getAccountID());
+			stmt.setString(2, acc.getPassword());
+			stmt.setString(3, acc.getName());
+			stmt.setString(4, acc.getEmail());
+			stmt.executeUpdate();
+			stmt.close();
+			con.close();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+	}
+	public boolean setName(String uid,String name) {
+		String sql = "update Account set name = ? where Uid = ? ";
+		try {
+			Connection con = super.getConnection();
+			PreparedStatement stmt = con.prepareStatement(sql);
+			stmt.setString(1, name);
+			stmt.setString(2, uid);
+			stmt.executeUpdate();
+			stmt.close();
+			con.close();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+		
+	}
+	public boolean setPassword(String uid,String password) {
+		String sql = "update Account set password = ? where Uid = ? ";
+		try {
+			Connection con = super.getConnection();
+			PreparedStatement stmt = con.prepareStatement(sql);
+			stmt.setString(1, password);
+			stmt.setString(2, uid);
+			stmt.executeUpdate();
+			stmt.close();
+			con.close();
+			return true;
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return false;
+		
+	}
+	
+	
 }
--- src/data/Invitation.java
+++ src/data/Invitation.java
@@ -1,5 +1,32 @@
 package data;
 
 public class Invitation {
-
+	private String senderID;
+	private String receiverID;
+	private String documentID;
+	public Invitation(String senderID, String receiverID, String documentID) {
+		this.senderID = senderID;
+		this.receiverID = receiverID;
+		this.documentID = documentID;
+	}
+	public String getSenderID() {
+		return senderID;
+	}
+	public void setSenderID(String senderID) {
+		this.senderID = senderID;
+	}
+	public String getReceiverID() {
+		return receiverID;
+	}
+	public void setReceiverID(String receiverID) {
+		this.receiverID = receiverID;
+	}
+	public String getDocumentID() {
+		return documentID;
+	}
+	public void setDocumentID(String documentID) {
+		this.documentID = documentID;
+	}
+	
+	
 }
